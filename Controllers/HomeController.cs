4c7f87725fa1d3384ec532143726bc63dce4f6cd
Zachary Schaefer <zs619215@ohio.edu>
2020-01-26 22:04:22 -0500

    All changes for SD1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Schaefer4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About Me";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Information";
 
             return View();
         }
