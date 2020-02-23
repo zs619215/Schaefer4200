5caa7a3fc358b23160532ec081545190a78be96d
Zachary Schaefer <zs619215@ohio.edu>
2020-02-23 18:53:05 -0500

    updated 2/23

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,12 +7,56 @@ namespace Schaefer4200.Models
 {
     public class Student
     {
-        public int studentID { get; set; }
+     
+    
+        public System.Guid SID  { get; set; }
+        public int studentId { get; set; }
+ 
+        [Display (Name="First Name")]
+        [Required(ErrorMessage = "Student first name is required")]
+        [stringLength(20)]
+
         public string firstName { get; set; }
+
+        
+        [DisplayName ("Last Name")]
+        [Required]
+        [stringLength(20)]
+
+
         public string lastName { get; set; }
+        
+        [Display (Name="Most used email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter your most frequently used email address")]
+
         public string email { get; set; }
+
+        
+        [Display (Name="Mobile Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$"), 
+            ErrorMessage = "Phone numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
+       
+        //need to fix this error
         public string phoneNumber { get; set; }
 
+        
+        [Display (Name="Student Course Details")]
+        [Required(ErrorMessage = "Student course details is required")]
+        [stringLength(20)]
+
+
         public ICollection<CourseDetail> CourseDetail { get; set; }
+
+        
+
+   
     }
+
+    
 }
+
+
+
+    
